4d25993dc126018b1f68beade3e8129192e28e57
Zlata <zlata@ellery.ru>
2020-06-26 23:39:54 +0300

    test

---
+++ precache-manifest.3e05d5d781d741b91dfd8e2156f5e565.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d294dae7f7b289d6cb89d7a01ca03a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56dc4d384355f0c25feb",
+    "url": "/static/css/main.d16b0dc2.chunk.css"
+  },
+  {
+    "revision": "8d428ef83439d0748255",
+    "url": "/static/js/2.3271de14.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.3271de14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56dc4d384355f0c25feb",
+    "url": "/static/js/main.1427f5fa.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "1c508c0acfd25ed6fe1cd9dbf5278115",
+    "url": "/static/media/oles.1c508c0a.png"
+  },
+  {
+    "revision": "5ca0244328f0d06f3482fec046dc6b9a",
+    "url": "/static/media/puddle.5ca02443.png"
+  },
+  {
+    "revision": "c3e05119a6d8262184697607745c422d",
+    "url": "/static/media/skyscraper.c3e05119.png"
+  }
+]);
---
+++ precache-manifest.66ba6d14b45e2de5e5b5c6ea297047b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd642b5631c60f51b5c3fcb0cbdd902",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37f02dba0f958b75384f",
+    "url": "/static/css/main.d16b0dc2.chunk.css"
+  },
+  {
+    "revision": "8d428ef83439d0748255",
+    "url": "/static/js/2.3271de14.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.3271de14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f02dba0f958b75384f",
+    "url": "/static/js/main.dfe99f63.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "1c508c0acfd25ed6fe1cd9dbf5278115",
+    "url": "/static/media/oles.1c508c0a.png"
+  },
+  {
+    "revision": "5ca0244328f0d06f3482fec046dc6b9a",
+    "url": "/static/media/puddle.5ca02443.png"
+  },
+  {
+    "revision": "c3e05119a6d8262184697607745c422d",
+    "url": "/static/media/skyscraper.c3e05119.png"
+  }
+]);
